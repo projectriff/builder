4d8385a5293861765b75b86c592a0145c151e2a7
Eric Bottard <ebottard@pivotal.io>
2018-11-15 17:13:31 +0100

    Use build number instead of build commit
    Fixes #10

--- .travis/push-to-dockerhub.sh
+++ .travis/push-to-dockerhub.sh
@@ -5,8 +5,9 @@ set -o nounset
 set -o pipefail
 
 version=`cat VERSION`
+
 docker tag projectriff/builder:latest projectriff/builder:${version}
-docker tag projectriff/builder:latest projectriff/builder:${version}-ci-${TRAVIS_COMMIT}
+docker tag projectriff/builder:latest projectriff/builder:${version}-ci-${TRAVIS_BUILD_NUMBER}
 
 docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 docker push projectriff/builder
