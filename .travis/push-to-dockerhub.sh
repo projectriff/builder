0e119f6dba6a6b3a2c64cd31015c3af4d6652419
Eric Bottard <ebottard@pivotal.io>
2018-11-15 17:57:58 +0100

    Use some digest of the image, as travis build numbers are re-used when one presses "Rebuild"...

--- .travis/push-to-dockerhub.sh
+++ .travis/push-to-dockerhub.sh
@@ -5,9 +5,11 @@ set -o nounset
 set -o pipefail
 
 version=`cat VERSION`
+id=`docker image inspect projectriff/builder --format '{{.Id}}' | awk '{print substr($1, 8, 12)}'`
+export CI_TAG="${version}-ci-${id}"
 
 docker tag projectriff/builder:latest projectriff/builder:${version}
-docker tag projectriff/builder:latest projectriff/builder:${version}-ci-${TRAVIS_BUILD_ID}
+docker tag projectriff/builder:latest projectriff/builder:${CI_TAG}
 
 docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 docker push projectriff/builder
