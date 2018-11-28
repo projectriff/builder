4bcde8f9fb21f3913cd9abc3ccc7993588764515
Eric Bottard <ebottard@pivotal.io>
2018-11-28 15:18:47 +0100

    Also publish a BuildTemplate with just the semver number

--- .travis/push-buildtemplate-to-gcs.sh
+++ .travis/push-buildtemplate-to-gcs.sh
@@ -11,7 +11,9 @@ CI_TAG="${version}-ci-${id}"
 gcloud auth activate-service-account --key-file <(echo $GCLOUD_CLIENT_SECRET | base64 --decode)
 
 sed "s|projectriff/builder:latest|projectriff/builder:${CI_TAG}|" riff-cnb-buildtemplate.yaml > riff-cnb-buildtemplate-${CI_TAG}.yaml
+sed "s|projectriff/builder:latest|projectriff/builder:${version}|" riff-cnb-buildtemplate.yaml > riff-cnb-buildtemplate-${version}.yaml
 
 gsutil cp -a public-read riff-cnb-buildtemplate-${CI_TAG}.yaml gs://projectriff/riff-buildtemplate/
+gsutil cp -a public-read riff-cnb-buildtemplate-${version}.yaml gs://projectriff/riff-buildtemplate/
 gsutil cp -a public-read riff-cnb-buildtemplate.yaml gs://projectriff/riff-buildtemplate/
 
