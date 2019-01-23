4693466756265bc179786b428a3f8a114a2d9c67
Scott Andrews <sandrews@pivotal.io>
2019-01-23 15:10:16 -0500

    Squashed commit of the following:
    commit 6b2705e27a4bfc38bced0ee84a59d2de47a8984e
    Author: Scott Andrews <sandrews@pivotal.io>
    Date:   Wed Jan 23 15:09:01 2019 -0500
    
        Bump version to 0.2.0-snapshot
    
    commit 0639628a29ffe6314f2a5ccaa637e674b5fcafa6
    Author: Eric Bottard <ebottard@pivotal.io>
    Date:   Wed Jan 23 19:50:49 2019 +0100
    
        Use compatible version of packs/run in buildtemplate
    
    commit 90d39b2b9334916495ba105d59ad21e8e895c111
    Author: Eric Bottard <ebottard@pivotal.io>
    Date:   Wed Jan 23 11:07:18 2019 +0100
    
        Use npm BP @ 5c4c6ec2280.
    
        That version of the BP fixes https://github.com/cloudfoundry/npm-cnb/issues/5
    
    commit c17236a5919ddfe37a55cffd3ec4d63bfe422e11
    Author: Eric Bottard <ebottard@pivotal.io>
    Date:   Tue Dec 18 14:27:08 2018 +0100
    
        Upgrade to pack@0.0.8 and compatible language BPs.
    
        Fixes #16

--- .travis/install-pack.sh
+++ .travis/install-pack.sh
@@ -4,6 +4,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-wget -qO- https://github.com/buildpack/pack/releases/download/v0.0.5/pack-0.0.5-linux.tar.gz | tar xvz -C $HOME/bin
+wget -qO- https://github.com/buildpack/pack/releases/download/v0.0.8/pack-0.0.8-linux.tar.gz | tar xvz -C $HOME/bin
 export PATH="$HOME/bin:$PATH"
 
