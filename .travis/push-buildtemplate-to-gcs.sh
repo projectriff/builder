5567e448e83d1fa69c361766a8734216c237f1b0
Scott Andrews <sandrews@pivotal.io>
2019-07-18 23:04:21 -0400

    Add timestamp and short gitsha to version slugs
    The new format is `{version}-{date}-{sha}`, for example
    `0.1.0-snapshot-201907190202-584c6a1d7b6e76f1`.
    
    The ID slug no longer is based the generated docker image id, as that
    value is not reproducible, and there should only be a single build per
    commit.

--- .travis/push-buildtemplate-to-gcs.sh
+++ .travis/push-buildtemplate-to-gcs.sh
@@ -4,37 +4,38 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-version=`cat VERSION`
-branch="${TRAVIS_BRANCH}"
-id=`docker image inspect projectriff/builder --format '{{.Id}}' | awk '{print substr($1, 8, 12)}'`
-CI_TAG="${version}-ci-${id}"
+readonly version=$(cat VERSION)
+readonly git_branch=${TRAVIS_BRANCH}
+readonly git_sha=$(git rev-parse HEAD)
+readonly git_timestamp=$(TZ=UTC git show --quiet --date='format-local:%Y%m%d%H%M%S' --format="%cd")
+readonly slug=${version}-${git_timestamp}-${git_sha:0:16}
 
 gcloud auth activate-service-account --key-file <(echo $GCLOUD_CLIENT_SECRET | base64 --decode)
 
 # function build template
 echo "Publish function builder"
 
-sed "s|projectriff/builder:latest|projectriff/builder:${CI_TAG}|" riff-function-clusterbuildtemplate.yaml > riff-function-clusterbuildtemplate-${CI_TAG}.yaml
+sed "s|projectriff/builder:latest|projectriff/builder:${slug}|" riff-function-clusterbuildtemplate.yaml > riff-function-clusterbuildtemplate-${slug}.yaml
 sed "s|projectriff/builder:latest|projectriff/builder:${version}|" riff-function-clusterbuildtemplate.yaml > riff-function-clusterbuildtemplate-${version}.yaml
 
-gsutil cp -a public-read riff-function-clusterbuildtemplate-${CI_TAG}.yaml gs://projectriff/riff-buildtemplate/
+gsutil cp -a public-read riff-function-clusterbuildtemplate-${slug}.yaml gs://projectriff/riff-buildtemplate/
 gsutil cp -a public-read riff-function-clusterbuildtemplate-${version}.yaml gs://projectriff/riff-buildtemplate/
-gsutil cp -a public-read riff-function-clusterbuildtemplate-${CI_TAG}.yaml gs://projectriff/riff-buildtemplate/riff-function-clusterbuildtemplate.yaml
+gsutil cp -a public-read riff-function-clusterbuildtemplate-${slug}.yaml gs://projectriff/riff-buildtemplate/riff-function-clusterbuildtemplate.yaml
 
 # application build template
 echo "Publish application builder"
 
-gsutil cp -a public-read riff-application-clusterbuildtemplate.yaml gs://projectriff/riff-buildtemplate/riff-application-clusterbuildtemplate-${CI_TAG}.yaml
+gsutil cp -a public-read riff-application-clusterbuildtemplate.yaml gs://projectriff/riff-buildtemplate/riff-application-clusterbuildtemplate-${slug}.yaml
 gsutil cp -a public-read riff-application-clusterbuildtemplate.yaml gs://projectriff/riff-buildtemplate/riff-application-clusterbuildtemplate-${version}.yaml
 gsutil cp -a public-read riff-application-clusterbuildtemplate.yaml gs://projectriff/riff-buildtemplate/riff-application-clusterbuildtemplate.yaml
 
 # update version references
 echo "Publish builder references"
 
-gsutil -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp -a public-read <(echo "${CI_TAG}") gs://projectriff/riff-buildtemplate/versions/builds/${branch}
-gsutil -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp -a public-read <(echo "${CI_TAG}") gs://projectriff/riff-buildtemplate/versions/builds/${version}
+gsutil -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp -a public-read <(echo "${slug}") gs://projectriff/riff-buildtemplate/versions/builds/${git_branch}
+gsutil -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp -a public-read <(echo "${slug}") gs://projectriff/riff-buildtemplate/versions/builds/${version}
 if [[ ${version} != *"-snapshot" ]] ; then
-  gsutil -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp -a public-read <(echo "${CI_TAG}") gs://projectriff/riff-buildtemplate/versions/releases/${branch}
+  gsutil -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp -a public-read <(echo "${slug}") gs://projectriff/riff-buildtemplate/versions/releases/${git_branch}
   # avoids overwriting existing values
-  gsutil -n -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp -a public-read <(echo "${CI_TAG}") gs://projectriff/riff-buildtemplate/versions/releases/${version}
+  gsutil -n -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp -a public-read <(echo "${slug}") gs://projectriff/riff-buildtemplate/versions/releases/${version}
 fi
--- .travis/push-to-dockerhub.sh
+++ .travis/push-to-dockerhub.sh
@@ -4,13 +4,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-version=`cat VERSION`
-id=`docker image inspect projectriff/builder --format '{{.Id}}' | awk '{print substr($1, 8, 12)}'`
-CI_TAG="${version}-ci-${id}"
+readonly version=$(cat VERSION)
+readonly git_branch=${TRAVIS_BRANCH}
+readonly git_sha=$(git rev-parse HEAD)
+readonly git_timestamp=$(TZ=UTC git show --quiet --date='format-local:%Y%m%d%H%M%S' --format="%cd")
+readonly slug=${version}-${git_timestamp}-${git_sha:0:16}
 
 docker tag projectriff/builder:latest projectriff/builder:${version}
-docker tag projectriff/builder:latest projectriff/builder:${CI_TAG}
+docker tag projectriff/builder:latest projectriff/builder:${slug}
 
 docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 docker push projectriff/builder:${version}
-docker push projectriff/builder:${CI_TAG}
+docker push projectriff/builder:${slug}
