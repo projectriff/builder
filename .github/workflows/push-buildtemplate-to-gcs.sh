ae09ed2004e1d6a8af36c0bcb057e8dc7eabc161
Scott Andrews <sandrews@pivotal.io>
2019-10-09 14:13:20 -0400

    Switch from Travis to GitHub Actions (#81)

--- .github/workflows/push-buildtemplate-to-gcs.sh
+++ .github/workflows/push-buildtemplate-to-gcs.sh
@@ -5,12 +5,10 @@ set -o nounset
 set -o pipefail
 
 readonly version=$(cat VERSION)
-readonly git_branch=${TRAVIS_BRANCH}
+readonly git_branch=${1:11} # drop 'refs/head/' prefix
 readonly git_sha=$(git rev-parse HEAD)
 readonly git_timestamp=$(TZ=UTC git show --quiet --date='format-local:%Y%m%d%H%M%S' --format="%cd")
 readonly slug=${version}-${git_timestamp}-${git_sha:0:16}
-
-gcloud auth activate-service-account --key-file <(echo $GCLOUD_CLIENT_SECRET | base64 --decode)
 
 # function build template
 echo "Publish function builder"
--- .github/workflows/push-to-dockerhub.sh
+++ .github/workflows/push-to-dockerhub.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 
 readonly version=$(cat VERSION)
-readonly git_branch=${TRAVIS_BRANCH}
+readonly git_branch=${1:11} # drop 'refs/head/' prefix
 readonly git_sha=$(git rev-parse HEAD)
 readonly git_timestamp=$(TZ=UTC git show --quiet --date='format-local:%Y%m%d%H%M%S' --format="%cd")
 readonly slug=${version}-${git_timestamp}-${git_sha:0:16}
@@ -13,6 +13,5 @@ readonly slug=${version}-${git_timestamp}-${git_sha:0:16}
 docker tag projectriff/builder:latest projectriff/builder:${version}
 docker tag projectriff/builder:latest projectriff/builder:${slug}
 
-docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 docker push projectriff/builder:${version}
 docker push projectriff/builder:${slug}
