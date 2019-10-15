40fe496ecaf491c95cc8d6dc757f95db0504dba4
Scott Andrews <sandrews@pivotal.io>
2019-10-15 18:56:25 -0400

    Drop Knative BuildTemplate support (#82)
    Knative Build has been deprecated for a while and is no longer
    maintained. riff has switch to kpack. There's no need to continue to
    publish buildtemplates for Knative.

--- .github/workflows/push-buildtemplate-to-gcs.sh
+++ .github/workflows/push-buildtemplate-to-gcs.sh
@@ -10,18 +10,8 @@ readonly git_sha=$(git rev-parse HEAD)
 readonly git_timestamp=$(TZ=UTC git show --quiet --date='format-local:%Y%m%d%H%M%S' --format="%cd")
 readonly slug=${version}-${git_timestamp}-${git_sha:0:16}
 
-# function build template
 echo "Publish function builder"
 
-## knative build (deprecated)
-sed "s|projectriff/builder:latest|projectriff/builder:${slug}|" riff-function-clusterbuildtemplate.yaml > riff-function-clusterbuildtemplate-${slug}.yaml
-sed "s|projectriff/builder:latest|projectriff/builder:${version}|" riff-function-clusterbuildtemplate.yaml > riff-function-clusterbuildtemplate-${version}.yaml
-
-gsutil cp -a public-read riff-function-clusterbuildtemplate-${slug}.yaml gs://projectriff/riff-buildtemplate/
-gsutil cp -a public-read riff-function-clusterbuildtemplate-${version}.yaml gs://projectriff/riff-buildtemplate/
-gsutil cp -a public-read riff-function-clusterbuildtemplate-${slug}.yaml gs://projectriff/riff-buildtemplate/riff-function-clusterbuildtemplate.yaml
-
-## kpack
 sed "s|projectriff/builder:latest|projectriff/builder:${slug}|" riff-function-clusterbuilder.yaml > riff-function-clusterbuilder-${slug}.yaml
 sed "s|projectriff/builder:latest|projectriff/builder:${version}|" riff-function-clusterbuilder.yaml > riff-function-clusterbuilder-${version}.yaml
 
@@ -29,15 +19,8 @@ gsutil cp -a public-read riff-function-clusterbuilder-${slug}.yaml gs://projectr
 gsutil cp -a public-read riff-function-clusterbuilder-${version}.yaml gs://projectriff/riff-buildtemplate/
 gsutil cp -a public-read riff-function-clusterbuilder-${slug}.yaml gs://projectriff/riff-buildtemplate/riff-function-clusterbuilder.yaml
 
-# application build template
 echo "Publish application builder"
 
-## knative build (deprecated)
-gsutil cp -a public-read riff-application-clusterbuildtemplate.yaml gs://projectriff/riff-buildtemplate/riff-application-clusterbuildtemplate-${slug}.yaml
-gsutil cp -a public-read riff-application-clusterbuildtemplate.yaml gs://projectriff/riff-buildtemplate/riff-application-clusterbuildtemplate-${version}.yaml
-gsutil cp -a public-read riff-application-clusterbuildtemplate.yaml gs://projectriff/riff-buildtemplate/riff-application-clusterbuildtemplate.yaml
-
-## kpack
 gsutil cp -a public-read riff-application-clusterbuilder.yaml gs://projectriff/riff-buildtemplate/riff-application-clusterbuilder-${slug}.yaml
 gsutil cp -a public-read riff-application-clusterbuilder.yaml gs://projectriff/riff-buildtemplate/riff-application-clusterbuilder-${version}.yaml
 gsutil cp -a public-read riff-application-clusterbuilder.yaml gs://projectriff/riff-buildtemplate/riff-application-clusterbuilder.yaml
